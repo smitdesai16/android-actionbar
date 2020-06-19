6ef546f938266aebaad0f67b87b9cad57ea8499c
Smit Desai <smit.desai16@gmail.com>
2020-06-19 10:55:03 -0500

    Adding group menu

--- app/src/main/java/me/creatorguy/androidoverflowmenu/MainActivity.java
+++ app/src/main/java/me/creatorguy/androidoverflowmenu/MainActivity.java
@@ -2,7 +2,9 @@ package me.creatorguy.androidoverflowmenu;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,10 +12,14 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    ConstraintLayout clMain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        clMain = findViewById(R.id.clMain);
     }
 
     @Override
@@ -27,6 +33,27 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.menuMainSettings:
                 Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.menuMainGreen:
+                if(!item.isChecked()) {
+                    item.setChecked(true);
+                }
+                clMain.setBackgroundColor(Color.GREEN);
+                break;
+
+            case R.id.menuMainRed:
+                if(!item.isChecked()) {
+                    item.setChecked(true);
+                }
+                clMain.setBackgroundColor(Color.RED);
+                break;
+
+            case R.id.menuMainYellow:
+                if(!item.isChecked()) {
+                    item.setChecked(true);
+                }
+                clMain.setBackgroundColor(Color.YELLOW);
                 break;
 
             default:
