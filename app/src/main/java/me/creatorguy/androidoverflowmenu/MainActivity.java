dbd08f2f1f5d7c47bbee4b5afa27f92af16f2757
Smit Desai <smit.desai16@gmail.com>
2020-06-19 10:32:20 -0500

    Implementing Settings menu

--- app/src/main/java/me/creatorguy/androidoverflowmenu/MainActivity.java
+++ app/src/main/java/me/creatorguy/androidoverflowmenu/MainActivity.java
@@ -1,8 +1,12 @@
 package me.creatorguy.androidoverflowmenu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuMainSettings:
+                Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                break;
+        }
+        return true;
     }
 }
